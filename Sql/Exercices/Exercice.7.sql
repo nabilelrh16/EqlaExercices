943681c8e2a1c1817e5e334acdceffe0facf4482
nabilelrh16 <149297215+nabilelrh16@users.noreply.github.com>
2023-12-29 07:08:59 +0100

    Delete Sql/Exercices directory

--- Sql/Exercices/Exercice.7.sql
+++
@@ -1,4 +0,0 @@
-use BlindCode;
-Select Nom, Prenom
-From Eleve
-Where cp =1030;
--- Sql/Exercices/Exercice.8.sql
+++
@@ -1,8 +0,0 @@
-use BlindCode;
-Select Nom, Prenom, Naissance
-From Eleve
-WHERE Naissance > '1980-12-31';
-
-Select Nom, Prenom, Naissance
-From Eleve
-Where Naissance < '1980-01-01';
